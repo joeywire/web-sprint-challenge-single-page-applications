0c33b90416430270a295ef51d2e5962f037ee92e
Joe L <joseph.lasata@gmail.com>
2020-10-16 10:22:37 -0500

    Routes built and nav link functional

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from "react";
+import './App.css'
+import Home from "./components/Home"; 
+import Form from "./components/Form";
+import Confirmation from "./components/Confimation";
+import {Route, Link, Switch} from 'react-router-dom'
 
 const App = () => {
   return (
-    <>
-      <h1>Lambda Eats</h1>
-    </>
+    <div className="App">
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/pizza">Order A Pizza</Link>
+      </nav>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route>
+          <Form path="/pizza"/>
+        </Route>
+      </Switch>
+    </div>
   );
 };
 export default App;
